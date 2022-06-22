da5ffbb40c8982af93d4f56dc3fd76ad11ae410f
WINCTRL-ELML45I\user <chanda.dipak950@gmail.com>
2022-06-22 20:52:03 +0530

    Edits

--- goto/index.php
+++ goto/index.php
@@ -1,3 +1,5 @@
 <?php
- header("Location:https://bit.ly/3mXw6xd");
+ //header("Location:https://bit.ly/3mXw6xd");
+ header("Location:https://541fc6z56zn27o35pcmgkdkxbf.hop.clickbank.net");
+ 
  exit;
