3e558c2337e8e22473bc7b5903938d5eee62a3c2
WINCTRL-ELML45I\user <chanda.dipak950@gmail.com>
2022-06-23 19:56:48 +0530

    Edits

--- goto/index.php
+++ goto/index.php
@@ -1,5 +1,4 @@
 <?php
- //header("Location:https://bit.ly/3mXw6xd");
- header("Location:https://541fc6z56zn27o35pcmgkdkxbf.hop.clickbank.net");
- 
+ header("Location:https://bit.ly/3mXw6xd");
+ //header("Location:https://541fc6z56zn27o35pcmgkdkxbf.hop.clickbank.net");
  exit;
